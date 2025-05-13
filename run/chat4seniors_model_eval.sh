ebe4e94fdb74c9ce8c2032789be16c88a6029fa4
YJY131248 <1135195062@qq.com>
2025-05-14 00:29:29 +0800

    update dpo eval script

--- run/chat4seniors_model_eval.sh
+++ run/chat4seniors_model_eval.sh
@@ -2,11 +2,150 @@ python3 ../src/evaluate.py \
     --peft_type lora \
     --task_type qa \
     --llm_model_name Mistral \
-    --llm_model_path ../model/chat4seniors_model \
+    --llm_model_path ../model/chat4seniors_model/mistral \
     --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
-    --save_eval_res_path ../out/chat4seniors_model/mistral_dpo_b0.5_lr1e-5_e5/eval_res/eval_res.csv \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-mistral/eval_res_dpo.csv \
     --log_path ../log/chat4seniors_model/mistral/dpo_model_eval.log \
     --use_peft_model True \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Mistral \
+    --llm_model_path ../model/base_models/Mistral-7B-Instruct-v0.2 \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-mistral/eval_res_hf.csv \
+    --log_path ../log/chat4seniors_model/mistral/hf_model_eval.log \
+    --use_peft_model False \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Qwen \
+    --llm_model_path ../model/chat4seniors_model/qwen \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-qwen/eval_res_dpo.csv \
+    --log_path ../log/chat4seniors_model/qwen/dpo_model_eval.log \
+    --use_peft_model True \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Qwen \
+    --llm_model_path ../model/base_models/Qwen2.5-7B-Instruct \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-qwen/eval_res_hf.csv \
+    --log_path ../log/chat4seniors_model/qwen/hf_model_eval.log \
+    --use_peft_model False \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Llama \
+    --llm_model_path ../model/chat4seniors_model/llama \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-llama/eval_res_dpo.csv \
+    --log_path ../log/chat4seniors_model/llama/dpo_model_eval.log \
+    --use_peft_model True \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Llama \
+    --llm_model_path ../model/base_models/Llama-3-8B \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-llama/eval_res_hf.csv \
+    --log_path ../log/chat4seniors_model/llama/hf_model_eval.log \
+    --use_peft_model False \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name BaiChuan \
+    --llm_model_path ../model/chat4seniors_model/baichuan \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-baichuan/eval_res_dpo.csv \
+    --log_path ../log/chat4seniors_model/baichuan/dpo_model_eval.log \
+    --use_peft_model True \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name BaiChuan \
+    --llm_model_path ../model/base_models/Baichuan2-7B-Base \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-baichuan/eval_res_hf.csv \
+    --log_path ../log/chat4seniors_model/baichuan/hf_model_eval.log \
+    --use_peft_model False \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Yi \
+    --llm_model_path ../model/chat4seniors_model/yi \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-yi/eval_res_dpo.csv \
+    --log_path ../log/chat4seniors_model/yi/dpo_model_eval.log \
+    --use_peft_model True \
+    --max_new_tokens 1024 \
+    --do_sample False \
+    --top_p 0.9 \
+    --temperature 0.7 \
+    --repetition_penalty 1.2
+
+python3 ../src/evaluate.py \
+    --peft_type lora \
+    --task_type qa \
+    --llm_model_name Yi \
+    --llm_model_path ../model/base_models/Yi-1.5-6B \
+    --dataset_path ../data/trainset/chat4seniors_dpo_trainset.json \
+    --save_eval_res_path ../out/chat4seniors_model/gridsearch-yi/eval_res_hf.csv \
+    --log_path ../log/chat4seniors_model/yi/hf_model_eval.log \
+    --use_peft_model False \
     --max_new_tokens 1024 \
     --do_sample False \
     --top_p 0.9 \
--- run/chat4seniors_model_lora_merge.sh
+++ run/chat4seniors_model_lora_merge.sh
@@ -1,8 +1,39 @@
 python3 ../src/lora_merge.py \
-    --peft_type lora \
-    --llm_model_name Mistral \
-    --llm_model_path ../model/base_models/Mistral-7B-Instruct-v0.2 \
-    --peft_checkpoint_path ../out/chat4seniors_model/mistral_dpo_b0.5_lr1e-5_e5/checkpoint-4855/ \
-    --merge_save_path ../model/chat4seniors_model \
+    --peft_type Mistral \
+    --llm_model_name Qwen \
+    --llm_model_path ../model/base_models/mistral \
+    --peft_checkpoint_path ../out/chat4seniors_model/gridsearch-mistral/mistral_dpo_b0.5_lr1e-4_e5/checkpoint-1942/ \
+    --merge_save_path ../model/chat4seniors_model/mistral \
     --log_path ../log/chat4seniors_model/mistral/lora_model_merge.log
-    
+
+python3 ../src/lora_merge.py \
+    --peft_type lora \
+    --llm_model_name Qwen \
+    --llm_model_path ../model/base_models/Qwen2.5-7B-Instruct \
+    --peft_checkpoint_path ../out/chat4seniors_model/gridsearch-qwen/qwen_dpo_b0.5_lr1e-4_e2/checkpoint-1942/ \
+    --merge_save_path ../model/chat4seniors_model/qwen \
+    --log_path ../log/chat4seniors_model/qwen/lora_model_merge.log
+
+python3 ../src/lora_merge.py \
+    --peft_type lora \
+    --llm_model_name Llama \
+    --llm_model_path ../model/base_models/Llama-3-8B \
+    --peft_checkpoint_path ../out/chat4seniors_model/gridsearch-llama/llama_dpo_b0.5_lr1e-4_e2/checkpoint-1942/ \
+    --merge_save_path ../model/chat4seniors_model/llama \
+    --log_path ../log/chat4seniors_model/llama/lora_model_merge.log
+
+python3 ../src/lora_merge.py \
+    --peft_type lora \
+    --llm_model_name BaiChuan \
+    --llm_model_path ../model/base_models/Baichuan2-7B-Base \
+    --peft_checkpoint_path ../out/chat4seniors_model/gridsearch-baichuan/baichuan_dpo_b0.2_lr5e-5_e2/checkpoint-1942/ \
+    --merge_save_path ../model/chat4seniors_model/baichuan \
+    --log_path ../log/chat4seniors_model/baichuan/lora_model_merge.log
+
+python3 ../src/lora_merge.py \
+    --peft_type lora \
+    --llm_model_name Yi \
+    --llm_model_path ../model/base_models/Yi-1.5-6B \
+    --peft_checkpoint_path ../out/chat4seniors_model/gridsearch-yi/yi_dpo_b0.5_lr1e-4_e2/checkpoint-1942/ \
+    --merge_save_path ../model/chat4seniors_model/yi \
+    --log_path ../log/chat4seniors_model/yi/lora_model_merge.log
