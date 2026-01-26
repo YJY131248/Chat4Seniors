55b8ae9c38d3e48d906a0a9fc278ec6315e5d151
YJY131248 <1135195062@qq.com>
2026-01-27 00:53:56 +0800

    feat: add rlvr setup script

---
+++ run/chat4seniors_rlvr_train.sh
@@ -0,0 +1,25 @@
+conda create -n verl python=3.10
+conda activate verl
+
+# install verl and dependencies
+sudo dnf install git -y
+cd /devdata/yaojinyu/Chat4Seniors/verl
+bash scripts/install_vllm_sglang_mcore.sh
+pip install --no-deps -e .
+
+# update env
+pip install tensordict
+pip install torchdata
+pip install msgspec
+pip install vllm==0.8.5.post1 --user
+pip install --upgrade "transformers>=4.51.0" --force-reinstall
+pip install nvidia-ml-py==11.495.46 
+
+# install model
+pip install modelscope
+modelscope download --model Qwen/Qwen2.5-7B-Instruct --local_dir /devdata/yaojinyu/Chat4Seniors/model/base_models/Qwen2.5-7B-Instruct
+W
+# train
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export PYTORCH_NVML_BASED_CUDA_CHECK=0
+bash /devdata/yaojinyu/Chat4Seniors/verl/personal/qwen2_5_7b_grpo.sh
